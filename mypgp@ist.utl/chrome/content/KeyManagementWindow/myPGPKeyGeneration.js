b57ed7c0f5188e6311a3ae1c38ec003ef91e151d
RodrigoL <rodrigo.jm.lourenco@gmail.com>
2013-11-24 22:32:42 +0000

    interfaces - Key Management e Generation

---
+++ mypgp@ist.utl/chrome/content/KeyManagementWindow/myPGPKeyGeneration.js
@@ -0,0 +1,99 @@
+Components.utils.import("resource://mypgp/mypgpCommon.jsm");
+
+
+<!-- Local Vars -->
+var infiniteKey = false;
+var key_size = 1024;
+var key_duration = 0;
+var key_owner = "";
+
+<!-- Account Manager -->
+var acctMgr = Components.classes["@mozilla.org/messenger/account-manager;1"]  
+                        .getService(Components.interfaces.nsIMsgAccountManager);  
+
+<!-- Input Components -->
+var input_infiniteKey = null;
+var input_expiration = null;
+var input_expirationoption = null;
+var input_keysize = null;
+var input_accounts = null;
+var warning = null;
+
+window.addEventListener("load", function(e){
+	input_infiniteKey = document.getElementById("keygen_infinitekey");
+	input_expiration = document.getElementById("keygen_expiration");
+	input_expirationoption = document.getElementById("keygen_exp_opts");
+	input_keysize = document.getElementById("keygen_keysize_list");
+	input_accounts = document.getElementById("keygen_accounts");
+
+	input_expiration.addEventListener("blur", updateKeyDuration);
+	input_expirationoption.addEventListener("select", updateKeyDuration);
+	input_keysize.addEventListener("select", updateKeySize);
+	input_accounts.addEventListener("select", updateKeyOwner);
+
+	warning = document.getElementById("warn_lbl");
+
+	<!-- Populate -->
+	var accounts = acctMgr.accounts;  
+	MypgpCommon.DEBUG_LOG("(KeyManagement) there are "+accounts.length+" registered accounts");
+	for (var i = 0; i < accounts.length; i++) {  
+  		var account = accounts.queryElementAt(i, Components.interfaces.nsIMsgAccount);  
+  		if(account.defaultIdentity != null){
+  			input_accounts.appendItem(account.defaultIdentity.fullName,
+  										account.defaultIdentity.email, "");
+  			MypgpCommon.DEBUG_LOG("(KeyManagement) Account "+i+" "+account.defaultIdentity.email);
+  		}
+	}
+
+}, false);
+
+
+function updateKeyOwner(){
+	warning.value="";
+	key_owner = input_accounts.selectedItem.value;
+
+	MypgpCommon.DEBUG_LOG("(KeyManagement) Key owner set to "+key_owner+"\n");
+}
+
+function updateKeyDuration(){
+	warning.value="";
+	key_duration = input_expiration.value * input_expirationoption.selectedItem.value;
+	MypgpCommon.DEBUG_LOG("(KeyManagement) Key set to last "+key_duration+" days\n");
+}
+
+function everlastingKey(){
+	warning.value="";
+	infiniteKey = input_infiniteKey.checked;
+	input_expiration.disabled = infiniteKey;
+	input_expirationoption.disabled = infiniteKey;
+	
+
+	if(infiniteKey)
+		MypgpCommon.DEBUG_LOG("(KeyManagement) Key set to last forever\n");
+	else
+		MypgpCommon.DEBUG_LOG("(KeyManagement) Key unset to last forever\n");
+}
+
+function updateKeySize(){
+	warning.value="";	
+	key_size = input_keysize.selectedItem.value;
+	MypgpCommon.DEBUG_LOG("(KeyManagement) Key size set to "+key_size+" \n");	
+}
+
+
+function generateNewKeyPair(){
+	
+	if(key_owner === ""){
+		warning.value = "Impossível criar a chave, esta deve estar associada a uma conta."
+		MypgpCommon.DEBUG_LOG("(KeyManagement) ... error creating key, exiting\n");
+		return;
+	}
+
+	if(key_duration <=0 && !infiniteKey){
+		warning.value= "Impossível criar a chave, esta deve ter uma duração associada ou deve ter duração infinita." <!-- TODO: devia estar a ir buscar o valor ao locale-->
+		MypgpCommon.DEBUG_LOG("(KeyManagement) ... error creating key, exiting\n");
+		return;
+	}
+
+	MypgpCommon.DEBUG_LOG("(KeyManagement) Creating key["+key_size+"] for "+key_owner+"\n");
+}
--- mypgp@ist.utl/chrome/content/KeyManagementWindow/myPGPKeyManagement.js
+++ mypgp@ist.utl/chrome/content/KeyManagementWindow/myPGPKeyManagement.js
@@ -2,109 +2,58 @@ Components.utils.import("resource://mypgp/mypgpCommon.jsm");
 
 
 <!-- Local Vars -->
-var infiniteKey = false;
-var key_size = 1024;
-var key_duration = 0;
-var key_owner = "";
 
-<!-- Account Manager -->
-var acctMgr = Components.classes["@mozilla.org/messenger/account-manager;1"]  
-                        .getService(Components.interfaces.nsIMsgAccountManager);  
+<!-- Manager Components -->
+var nsIFilePicker = null;
+var file_browser = null;
 
 <!-- Input Components -->
-var input_infiniteKey = null;
-var input_expiration = null;
-var input_expirationoption = null;
-var input_keysize = null;
-var input_accounts = null;
-var warning = null;
+
 
 window.addEventListener("load", function(e){
-	input_infiniteKey = document.getElementById("keymng_infinitekey");
-	input_expiration = document.getElementById("keymng_expiration");
-	input_expirationoption = document.getElementById("keymng_exp_opts");
-	input_keysize = document.getElementById("keymng_keysize_list");
-	input_accounts = document.getElementById("keymng_accounts");
 
-	input_expiration.addEventListener("blur", updateKeyDuration);
-	input_expirationoption.addEventListener("select", updateKeyDuration);
-	input_keysize.addEventListener("select", updateKeySize);
-	input_accounts.addEventListener("select", updateKeyOwner);
-
-	warning = document.getElementById("warn_lbl");
-
-	<!-- Populate -->
-	var accounts = acctMgr.accounts;  
-	MypgpCommon.DEBUG_LOG("(KeyManagement) there are "+accounts.length+" registered accounts");
-	for (var i = 0; i < accounts.length; i++) {  
-  		var account = accounts.queryElementAt(i, Components.interfaces.nsIMsgAccount);  
-  		if(account.defaultIdentity != null){
-  			input_accounts.appendItem(account.defaultIdentity.fullName,
-  										account.defaultIdentity.email, "");
-  			MypgpCommon.DEBUG_LOG("(KeyManagement) Account "+i+" "+account.defaultIdentity.email);
-  		}
-	}
+	MypgpCommon.DEBUG_LOG("(KeyManagement) Starting key management window\n");
+	
+	nsIFilePicker = Components.interfaces.nsIFilePicker;
+	file_browser = Components.classes["@mozilla.org/filepicker;1"].createInstance(nsIFilePicker);
+	<!-- TODO: fazer file_browser.appendFilter -->
 
 }, false);
 
 
-function updateKeyOwner(){
-	warning.value="";
-	key_owner = input_accounts.selectedItem.value;
 
-	MypgpCommon.DEBUG_LOG("(KeyManagement) Key owner set to "+key_owner+"\n");
-}
+function openKeyGeneratorWindow(){
 
-function updateKeyDuration(){
-	warning.value="";
-	key_duration = input_expiration.value * input_expirationoption.selectedItem.value;
-	MypgpCommon.DEBUG_LOG("(KeyManagement) Key set to last "+key_duration+" days\n");
-}
-
-function everlastingKey(){
-	warning.value="";
-	infiniteKey = input_infiniteKey.checked;
-	input_expiration.disabled = infiniteKey;
-	input_expirationoption.disabled = infiniteKey;
-	
-
-	if(infiniteKey)
-		MypgpCommon.DEBUG_LOG("(KeyManagement) Key set to last forever\n");
-	else
-		MypgpCommon.DEBUG_LOG("(KeyManagement) Key unset to last forever\n");
-}
-
-function updateKeySize(){
-	warning.value="";	
-	key_size = input_keysize.selectedItem.value;
-	MypgpCommon.DEBUG_LOG("(KeyManagement) Key size set to "+key_size+" \n");	
-}
-
-
-function createNewKeyPair(){
-
-	if(key_duration <=0 && !infiniteKey){
-		warning.value= "Impossível criar a chave, esta deve ter uma duração associada ou deve ter duração infinita." <!-- TODO: devia estar a ir buscar o valor ao locale-->
-		MypgpCommon.DEBUG_LOG("(KeyManagement) ... error creating key, exiting\n");
-		return;
-	}
-
-	if(key_owner === ""){
-		warning.value = "Impossível criar a chave, esta deve estar associada a uma conta."
-		MypgpCommon.DEBUG_LOG("(KeyManagement) ... error creating key, exiting\n");
-		return;
-	}
-
-	MypgpCommon.DEBUG_LOG("(KeyManagement) Creating key["+key_size+"] for "+key_owner+"\n");
-
+	window.openDialog("chrome://mypgp/content/KeyManagementWindow/myPGPKeyGeneration.xul",
+					"&mypgp.keymng.keygen;",
+					null);
+	MypgpCommon.DEBUG_LOG("(KeyManagement) Opening key generation window\n");
 }
 
 function importKeysFromFile(){
-	MypgpCommon.DEBUG_LOG("(KeyManagement) Import keys from file\n");
-	alert("TODO: must be implemented");	
+
+	MypgpCommon.DEBUG_LOG("(KeyManagement) Initiating key file import... [TODO]\n");
+
+	file_browser.init(window, "Importar Par de Chaves", nsIFilePicker.modeOpen); <!-- TODO: obter o nome da janela no locale -->
+
+	var result = file_browser.show();
+
+	if(result != nsIFilePicker.resultCancel){
+		MypgpCommon.DEBUG_LOG("(KeyManagement) ... importing file ... [TODO]\n");		
+	}else
+		MypgpCommon.DEBUG_LOG("(KeyManagement) ... canceling import\n");
 }
 
 function exportKeysToFile(){
+
 	MypgpCommon.DEBUG_LOG("(KeyManagement) Export keys to file\n");
-	alert("TODO: must be implemented");	
+	file_browser.init(window, "Exportar Par de Chaves", nsIFilePicker.modeSave); <!-- TODO: obter o nome da janela no locale -->
+
+	var result = file_browser.show();
+
+	if(result != nsIFilePicker.resultCancel){
+		MypgpCommon.DEBUG_LOG("(KeyManagement) ... exporting to file ... [TODO]\n");
+	}else
+		MypgpCommon.DEBUG_LOG("(KeyManagement) ... canceling keys export.\n");
+
 }
